f4bf760af64c8883fad1dc8d60c8ca14fef92255
haolin <c21325063@mytudublin.ie>
2024-11-07 16:44:15 +0000

    local request

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-OPSFV5AM.mjs";import"./chunk-R6OSMJKK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-RISLPOEN.mjs";import"./chunk-R6OSMJKK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/food.service.ts
+++ src/app/food.service.ts
@@ -6,14 +6,47 @@ import { Food } from './food';
 export class FoodService {
   url = 'http://localhost:3000/food';
   constructor() { }
-  
+  food : Food[] = [
+    {
+      id: 1,
+      name: "Apple",
+      photo: "assets/apple.jpg",
+      description: "A fruit that is good for your health",
+      calories: "100",
+      protein: "200",
+      carbs: "300",
+      fat: "123"
+    },
+    {
+      id: 2,
+      name: "Banana",
+      photo: "assets/banana.jpg",
+      description: "A fruit that is good for your health",
+      calories: "100",
+      protein: "200",
+      carbs: "300",
+      fat: "123"
+    },
+    {
+      id: 3,
+      name: "Orange",
+      photo: "assets/orange.jpg",
+      description: "A fruit that is good for your health",
+      calories: "100",
+      protein: "200",
+      carbs: "300",
+      fat: "123"
+    }
+  ]
   async getFoodList(): Promise<Food[]> {
-    const data = await fetch(this.url);
-    return (await data.json()) ?? [];
+    // const data = await fetch(this.url);
+    // return (await data.json()) ?? [];
+    return this.food;
   }
   async getFoodById(id: number): Promise<Food | undefined> {
-    const data = await fetch(`${this.url}/${id}`);
-    return (await data.json()) ?? {};
+    // const data = await fetch(`${this.url}/${id}`);
+    // return (await data.json()) ?? {};
+    return this.food.find(food => food.id === id);
   }
 
   Login(username: String, password: String) {
